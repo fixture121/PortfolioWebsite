0c29c71db47af1305c1bcf1f26e29335fe44a64b
fixture121 <94148106+fixture121@users.noreply.github.com>
2024-06-04 00:07:39 -0400

    Added dark mode icons to images folder. Added new CSS styling for dark mode. Added new JavaScript for toggling between light and dark mode. Updated HTML tags.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
 function toggleMenu() {
-    // Target the .menu-links and .hamburger-icon elements so that when they are clicked, different styles are applied
+  // Target the .menu-links and .hamburger-icon elements so that when they are clicked, different styles are applied
   const menu = document.querySelector('.menu-links');
   const icon = document.querySelector('.hamburger-icon');
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+  menu.classList.toggle('open');
+  icon.classList.toggle('open');
 }
+
+// When the moon icon is clicked, the website switches to dark mode
+var moon = document.getElementById('moon');
+moon.onclick = function () {
+  document.body.classList.toggle('dark-mode');
+  if (document.body.classList.contains('dark-mode')) {
+    moon.src = 'images/sun.png';
+  } else {
+    moon.src = 'images/moon.png';
+  }
+
+  // Change social media icons based on the mode
+  const icons = document.querySelectorAll('.icon');
+  icons.forEach((icon) => {
+    const iconName = icon.dataset.name;
+    if (isDarkMode) {
+      icon.src = `images/${github}.png`;
+    } else {
+      icon.src = `images/${linkedin}.png`;
+    }
+  });
+};
